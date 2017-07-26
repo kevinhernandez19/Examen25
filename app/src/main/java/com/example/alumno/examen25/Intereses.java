6f2a66eb9ecaa44bc0f062ae2ea8847ca808260e
U. Mariano Gálvez <alumno@labs.academico>
2017-07-25 20:42:21 -0600

    Initial commit

---
+++ app/src/main/java/com/example/alumno/examen25/Intereses.java
@@ -0,0 +1,15 @@
+package com.example.alumno.examen25;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Intereses extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intereses);
+    }
+}
